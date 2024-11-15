d174e57450f152517882e5f19c3a4680d7905b2f
SqoZy <117159918+SqoZy@users.noreply.github.com>
2024-11-15 10:21:47 +0100

    pages toegevoegd voor de post en gekut met de images

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,10 +2,10 @@ import { defineCollection, z } from "astro:content";
 import { date } from "astro:schema";
 
 const postCollection = defineCollection({
-    schema: z.object({
+    schema: ({ image }) => z.object({
         author: z.string(),
         date: z.string(),
-        image: z.string(),
+        image: image(),
         title: z.string(),
     })
 })
